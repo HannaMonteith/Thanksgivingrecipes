ac518b5d0f2310b76bccf9d737258e5bdc4324d1
HannaMonteith <hmonteit@uvm.edu>
2024-11-06 10:47:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+// when user clicks on the button, all the inputs gather
+const addSendyourrecipeButton = document.getElementById;
+addSendyourrecipeButton.addEventListener('click', addSendyourrecipe, false);
